27f93a33ffb4284f46d74ac21f624c31b19ff0b8
Aaron Afelumo <aaron.afelumo@pau.edu.ng>
2023-02-05 21:05:59 +0100

    AA

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Get, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getHello2(): {} {
+    return {message: 'Aaron is in!', title: 'Ayanfe'};
+    }
+  getHome(): {} {
+      return {title: 'Home Page'};
+      }
+  getAboutUs(): {} {
+      return {title: 'About Us Page'};
+      } 
 }
